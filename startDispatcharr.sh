df7b1fc666f78b8e89f0e9f975137b2143ca7a33
justyn Temme <justyntemme@gmail.com>
2025-11-19 16:45:09 -0600

    final options

--- startDispatcharr.sh
+++ startDispatcharr.sh
@@ -1,6 +1,8 @@
 docker run -d \
-  --net host \
-  -p 9191:9191 \
-  --name dispatcharr \
-  -v /home/user/config/.dispatcharr:/data \
+  --net=host \
+  --name=dispatcharr \
+  --device /dev/dri:/dev/dri \
+  -v /home/user/.config/dispatcharr:/data \
   ghcr.io/dispatcharr/dispatcharr:latest
+
+# -p 9191:9191 \
