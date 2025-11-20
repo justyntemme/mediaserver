77c8096659b32342a13d7b809d6ee769f9df8bc1
justyn Temme <justyntemme@gmail.com>
2025-11-20 15:04:41 -0600

    add restart for reboots

--- startDispatcharr.sh
+++ startDispatcharr.sh
@@ -3,6 +3,7 @@ docker run -d \
   --name=dispatcharr \
   --device /dev/dri:/dev/dri \
   -v /home/user/.config/dispatcharr:/data \
+  --restart unless-stopped \
   ghcr.io/dispatcharr/dispatcharr:latest
 
 # -p 9191:9191 \
